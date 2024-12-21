43ad7f517471fb9a3191496c0870f15a5e8c5162
lucas <lucas.lvn97439@gmail.com>
2024-12-21 22:41:36 +0500

    add PresetationSection.tsx and PresentationSection.css

---
+++ src/components/home-presentation/PresentationSection.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { PhoneMockeup } from "@/assets/PhoneMockeup";
+import { Button } from "../buttons/Button";
+import { PlayerIcon } from "@/icons/PlayerIcon";
+
+import "./PresentationSection.css";
+import { Title } from "../texts/Title";
+import { Description } from "../texts/Description";
+
+export function PresentationSection() {
+  return (
+    <div className="presentation">
+      <div className="left-part">
+        <Title text="The easiest way to manage projects" />
+        <Description text="From the small stuff to the big picture, organizes the work so teams know what to do, why it matters, and how to get it done." />
+
+        <div className="action flex gap-4">
+          <Button active text="Get Started" onClick={() => {}} />
+          <Button
+            text="Watch Video"
+            onClick={() => {}}
+            sideIcon={{ icon: <PlayerIcon />, side: "left" }}
+          />
+        </div>
+      </div>
+
+      <div className="right-part flex justify-center">
+        <PhoneMockeup />
+      </div>
+    </div>
+  );
+}
