0d9eff758a7620222035ebcaf5a0dbadddced01b
lucas <lucas.lvn97439@gmail.com>
2024-12-21 17:17:31 +0500

    add NavbarMobile component

---
+++ src/components/navbar/NavbarMobileMenu.tsx
@@ -0,0 +1,43 @@
+import { CloseIcon } from "@/icons/CloseIcon";
+import { MenuIcon } from "@/icons/MenuIcon";
+import { useRef } from "react";
+
+import "./NavbarMobile.css";
+import { MenuLinks } from "./MenuLinks";
+import { Footer } from "../Footer";
+import { ButtonIcon } from "../buttons/ButtonIcon";
+import { LogoLink } from "../LogoLink";
+import { DownloadLinks } from "./DownloadLinks";
+
+export function NavbarMobileMenu() {
+  const mobileContentRef = useRef<HTMLDivElement>(null);
+
+  function openMobileContentNav() {
+    if (mobileContentRef.current?.classList.contains("close")) {
+      mobileContentRef.current.classList.remove("close");
+      mobileContentRef.current.classList.add("open");
+    } else if (mobileContentRef.current?.classList.contains("open")) {
+      mobileContentRef.current.classList.remove("open");
+      mobileContentRef.current.classList.add("close");
+    }
+  }
+  return (
+    <div className="mobile-menu">
+      <ButtonIcon icon={<MenuIcon />} onClick={openMobileContentNav} />
+
+      <div className="mobile-menu-panel close" ref={mobileContentRef}>
+        <div className="mobile-menu-header">
+          <LogoLink />
+          <ButtonIcon icon={<CloseIcon />} onClick={openMobileContentNav} />
+        </div>
+
+        <div className="mobile-menu-content">
+          <MenuLinks />
+          <DownloadLinks vertical />
+        </div>
+
+        <Footer />
+      </div>
+    </div>
+  );
+}
