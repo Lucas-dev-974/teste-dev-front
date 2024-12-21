7c87190610d601c712758f4fd7973a71520dfa56
lucas <lucas.lvn97439@gmail.com>
2024-12-21 17:18:10 +0500

    add some icons

---
+++ src/icons/MenuIcon.tsx
@@ -0,0 +1,13 @@
+export function MenuIcon() {
+  return (
+    <svg viewBox="0 0 18 14" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M1 1H17M4 7H17M7 13H17"
+        stroke="#1D1D1D"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
