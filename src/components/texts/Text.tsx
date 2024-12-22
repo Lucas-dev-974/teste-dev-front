64587d8411f228f6051b585831ed16abb2c10ee6
lucas <lucas.lvn97439@gmail.com>
2024-12-22 19:37:36 +0500

    Text.tsx make color props optional

--- src/components/texts/Text.tsx
+++ src/components/texts/Text.tsx
@@ -1,6 +1,6 @@
 import "./Text.css";
 
-export function Text(props: { text: string; color: "dark" | "gray" }) {
+export function Text(props: { text: string; color?: "dark" | "gray" }) {
   const color = props.color === "dark" ? "dark" : "gray";
 
   return <p className={"text " + color}>{props.text}</p>;
