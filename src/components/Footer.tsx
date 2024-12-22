7503815e6b48822c02cf0c54df1db8f4c8637ddb
lucas <lucas.lvn97439@gmail.com>
2024-12-22 19:38:13 +0500

    add fixed props to Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,9 @@ import { Text } from "./texts/Text";
 import "./Footer.css";
 import { SocialNetworkLinks } from "./SocialNetworkLinks";
 
-export function Footer() {
+export function Footer(props: { fixed?: boolean }) {
   return (
-    <div className="footer">
+    <div className={"footer " + (props.fixed ? "fixed bottom-0" : "")}>
       <Text text="© 2021 Landify UI Kit. All rights reserved" color="gray" />
       <SocialNetworkLinks />
     </div>
