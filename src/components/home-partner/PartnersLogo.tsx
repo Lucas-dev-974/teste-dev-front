a3acd1f63c1333e5c8c93600a00d977c6ffb221c
lucas <lucas.lvn97439@gmail.com>
2024-12-22 16:04:56 +0500

    add partners logos

---
+++ src/components/home-partner/PartnersLogo.tsx
@@ -0,0 +1,33 @@
+import { AribnbLogo } from "@/assets/AirbnbLogo";
+import { AmazoneLogo } from "@/assets/AmazoneLogo";
+import { AtlassianLogo } from "@/assets/AtlassianLogo";
+import { BookMyShowLogo } from "@/assets/BookMyShowLogo";
+import { FedexLogo } from "@/assets/FedexLogo";
+import { GatsbyLogo } from "@/assets/GatsbyLogo";
+import { GoogleLogo } from "@/assets/GoogleLogo";
+import { GrubHubLogo } from "../../assets/GrubHubLogo";
+import { HubSpotLogo } from "@/assets/HubSpotLogo";
+import { MicrosoftLogo } from "@/assets/MicrosoftLogo";
+import { OLALogo } from "@/assets/OLALogo";
+import { OYOLogo } from "@/assets/OYOLogo";
+import { ServiceNowLogo } from "@/assets/SerivceNowLogo";
+import { StrapiLogo } from "@/assets/StrapiLogo";
+import { WalmartLogo } from "@/assets/WalmartLogo";
+
+export const PartnersLogos = [
+  AribnbLogo,
+  GoogleLogo,
+  BookMyShowLogo,
+  OYOLogo,
+  AmazoneLogo,
+  MicrosoftLogo,
+  FedexLogo,
+  WalmartLogo,
+  OLALogo,
+  HubSpotLogo,
+  GatsbyLogo,
+  StrapiLogo,
+  AtlassianLogo,
+  ServiceNowLogo,
+  GrubHubLogo,
+];
