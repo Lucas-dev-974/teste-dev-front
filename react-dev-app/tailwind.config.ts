6d3e5fae97e7507a3c323b009c8fd93899a2cb7e
lucas <lucas.lvn97439@gmail.com>
2024-12-23 12:34:50 +0500

    add color to tailwinds config

--- react-dev-app/tailwind.config.ts
+++ react-dev-app/tailwind.config.ts
@@ -12,6 +12,7 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
         primary: "#7C3AED",
+        yellow: "#FFE500",
       },
     },
   },
