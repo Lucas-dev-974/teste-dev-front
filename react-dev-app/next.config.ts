7567c0226e1e2c7523794f3801878e85732e42db
lucas <lucas.lvn97439@gmail.com>
2024-12-23 12:01:29 +0500

    build project into ../views

--- react-dev-app/next.config.ts
+++ react-dev-app/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  distDir: "../views", // Spécifie le dossier de sortie
 };
 
 export default nextConfig;
