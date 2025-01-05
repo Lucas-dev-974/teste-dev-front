70a9b8be416101bb756b44ec2d7d93c82bdefa06
lucas <lucas.lvn97439@gmail.com>
2025-01-05 18:14:09 +0500

    undo fix build dire for vercel deploy

--- react-dev-app/next.config.ts
+++ react-dev-app/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  // distDir: "../views", // Spécifie le dossier de sortie
+  distDir: "../views", // Spécifie le dossier de sortie
 };
 
 export default nextConfig;
